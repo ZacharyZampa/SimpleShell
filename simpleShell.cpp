f50b92b2ff8f48db80e4673bb1bf7ad4bd5f541f
Zachary Zampa <zackzampa@gmail.com>
2019-09-20 23:22:30 -0400

    initial commit

---
+++ simpleShell.cpp
@@ -0,0 +1,338 @@
+/*
+ * Copyright(C) 2019 zampaze@miamioh.edu
+ */
+
+/* 
+ * File:   zampaze_hw4.c++
+ * Author: zackz
+ *
+ * Created on September 18, 2019, 5:31 PM
+ * 
+ * Create a custom shell
+ */
+
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+using StrVec = std::vector<std::string>;
+
+/**
+ * Print the command in the vector
+ * @param vec String Vector
+ */
+void printCommand(StrVec& vec) {
+    std::cout << "Running:";
+    for (auto s : vec) {
+        std::cout << " " << s;
+    }
+    std::cout << std::endl;
+}
+
+/**
+ * Trim a string from front extraneous input and return it
+ * @param str
+ * @return 
+ */
+std::string trimmer(std::string& str) {
+    const std::string chars = "\t\n\v\f\r";  // what to look for
+    // trim the front of the string first (do not want extraneous chars read)
+
+    return str.erase(0, str.find_first_not_of(chars));
+}
+
+/**
+ * Checks if a string starts with a shell comment: # or is empty
+ * @param str
+ * @return true if it starts with a comment
+ */
+bool commentCheck(std::string& str) {
+    return (str == "" || str.front() == '#');
+}
+
+/**
+ * See if process SERIAL, PARALLEL file or regular
+ * If regular, put input back in buffer
+ * @param in
+ * @param type
+ */
+void whatProcess(std::istream& in, std::string& type) {
+    if (type != "SERIAL" && type != "PARALLEL") {
+        for (int i = type.size() - 1; i >= 0; i--) {
+            in.putback(type.at(i));
+        }
+    }
+}
+
+/**
+ * Check if a process will be PARALLEL SERIAL or regular
+ * @param is
+ * @return 
+ */
+char checkProcessType(std::istringstream& is) {
+    std::string type;
+    is >> std::quoted(type);
+    
+    if (type == "PARALLEL") {
+        return 'p';
+    } else if (type == "SERIAL") {
+        return 's';
+    } else if (type == "exit") {
+        return 'e';
+    } else {
+        whatProcess(is, type);
+        return 'r';
+    }
+}
+
+/**
+ * Convenience method to setup arguments (as pointers) 
+ * and call the execvp system call
+ * 
+ * @param argList The list of arguments with the 1st entry always
+ * being the path to the full command to be run.  
+ */
+void myExec(StrVec& argList) {
+    std::vector<char*> args;  // list of pointers to each word
+    for (auto& s : argList) {
+        args.push_back(&s[0]);  // address of 1st character in string
+    }
+    // add the nullptr to the end
+    args.push_back(nullptr);
+
+
+    execvp(args[0], &args[0]);
+}
+
+
+/**
+ * Run the actual shell process from the input commands
+ * @param inputVec
+ */
+void runShell(StrVec& inputVec) {
+    const int pid = fork();
+    int status;
+    if (pid == 0) {
+        // Child process: Run the Command
+        myExec(inputVec);  // Execute Commands
+        // here if fails
+        perror("execve failed");
+        exit(-1);  // force exit with the child program with error: failed
+    } else if (pid > 0) {
+        // Parent process: Wait for the Child
+        waitpid(pid, &status, 0);
+        std::cout << "Exit code: " << status << std::endl;
+    }
+}
+
+
+/**
+ * Run a fork in parallel
+ * @param pids
+ * @param indCmd
+ */
+void runParallelFork(std::vector<int>& pids, StrVec& indCmd) {
+    int pid = fork();
+    pids.push_back(pid);
+    if (pid == 0) {
+        // Child process: Run the Command
+        myExec(indCmd);  // Execute Commands
+        // here if fails
+        perror("execve failed");
+        exit(-1);  // force exit with the child program with error: failed
+    } 
+}
+
+/**
+ * Run the program in parallel when given the command
+ * @param args
+ */
+void parallel(StrVec& inputVec, char process) {
+    if (inputVec.size() == 0 || process != 'p') {
+        return;
+    }
+    // parallel so process
+    std::vector<int> pids;  // hold pids to call waitpid on each
+
+    // loop while commands to run exist
+    while (inputVec.size() > 0) {
+        StrVec indCmd;  // individual command string to run
+        for (size_t i = 0; i < inputVec.size();) {
+            // separate into individual command sequences
+            if (inputVec.at(i) == "next_command") {
+                inputVec.erase(inputVec.begin());
+                break;
+            }
+            // add to individual command vector
+            indCmd.push_back(inputVec.at(i));
+            inputVec.erase(inputVec.begin());  // remove from main input vector
+        }
+
+        printCommand(indCmd);  // print what will be ran
+
+        runParallelFork(pids, indCmd);
+    }
+
+
+    for (auto pid : pids) {
+        int status;
+        waitpid(pid, &status, 0);
+        std::cout << "Exit code: " << status << std::endl;
+    }
+}
+
+/**
+ * Run a program in SERIAL
+ * @param inputVec
+ * @param process
+ */
+void serial(StrVec& inputVec, char process) {
+    if (inputVec.size() == 0 || process != 's') {
+        return;
+    }
+    // serial so process
+    // loop while commands to run exist
+    while (inputVec.size() > 0) {
+        StrVec indCmd;  // individual command string to run
+        for (size_t i = 0; i < inputVec.size();) {
+            // separate into individual command sequences
+            if (inputVec.at(i) == "next_command") {
+                inputVec.erase(inputVec.begin());
+                break;
+            }
+            // add to individual command vector
+            indCmd.push_back(inputVec.at(i));
+            inputVec.erase(inputVec.begin());  // remove from main input vector
+        }
+        printCommand(indCmd);  // print what will be ran
+
+        runShell(indCmd);  // run the individual command
+    }
+}
+
+
+
+/**
+ * Process the given command
+ * @param inputVec
+ * @param process
+ */
+void processCommand(StrVec inputVec, char process) {
+    if (process == 'r') {
+        // regular process
+        runShell(inputVec);
+    } else if (process == 'p') {
+        // PARALLEL process
+        parallel(inputVec, process);
+    } else {
+        // SERIAL
+        serial(inputVec, process);
+    }
+}
+
+
+/**
+ * Process the input from a user and returns it as a String Vector
+ * @param input stream
+ * @return String vector
+ */
+StrVec inputProcessing(std::istream& in, char process) {
+    StrVec inputVec;  // command vector
+    std::string item;
+    if (process == 'r') {
+        // regular process - finish reading the single line
+        while (in >> std::quoted(item)) {
+            inputVec.push_back(item);
+        }
+    } else {
+        // process is PARALLEL or SERIAL - read from file line by line
+        std::string line;
+        while (std::getline(in, line)) {
+            line = trimmer(line);
+            if (line.size() == 0 || line.at(0) == '#') {
+                // skip this as it is a comment or empty line
+            } else {
+                std::istringstream is(line);
+                while (is >> std::quoted(item)) {
+                    inputVec.push_back(item);
+                }
+                // signal when reading command end
+                inputVec.push_back("next_command");  
+            }
+        }
+    }
+    
+    return inputVec;
+}
+
+/**
+ * The Main While Loop Process
+ * @param std::string line
+ * @param StrVec inputVec
+ * @return 
+ */
+void processLoop(std::string& line, StrVec& inputVec, bool& exit) {
+    if (commentCheck(line)) {
+        // this is a comment or blank line
+    } else {
+        // this is not a comment or blank line: PROCESS
+        // create stringstream of line
+        std::istringstream is(line);
+        
+        // check if serial or parallel then process accordingly
+        char process = checkProcessType(is);
+        if (process == 'p') {
+            // PARALLEL - process commands through file
+            std::string file;
+            is >> std::quoted(file);
+            std::ifstream in(file);
+            inputVec = inputProcessing(in, process);
+        } else if (process == 's') {
+            // SERIAL - process commands through file
+            std::string file;
+            is >> std::quoted(file);
+            std::ifstream in(file);
+            inputVec = inputProcessing(in, process);
+        } else if (process == 'r') {
+            // regular -- process this single line
+            inputVec = inputProcessing(is, process);
+            printCommand(inputVec);
+        } else {
+            // exit command
+            exit = true;
+            return;
+        }
+        processCommand(inputVec, process);
+    }  // end process block     
+}
+
+/*
+ * The launch point for the program
+ */
+int main(int argc, char** argv) {
+    std::string line;
+    while (std::cout << "> ", std::getline(std::cin, line)) {
+        StrVec inputVec;
+
+        line = trimmer(line);  // trim extraneous input
+
+        // check if comment or blank
+        bool exit = false;
+        processLoop(line, inputVec, exit);
+        
+        if (exit) {
+            // exit command given, quit
+            break;
+        }
+    }  // end while loop
+    return 0;
+}
+
